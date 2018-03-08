8959457ee9df4689c72593757dd5b9fdd9dff425
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-08 13:15:37 -0500

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVar;
+        myVar = 10;
+        System.out.println("Hello IntelliJ again");
+    }
+}
