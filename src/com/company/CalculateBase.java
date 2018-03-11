514ae4a64d05c0a77185039bfbd191ec25b14aff
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-11 16:16:20 -0400

    created an adder class to do addition operations.

--- src/com/company/CalculateBase.java
+++ src/com/company/CalculateBase.java
@@ -10,7 +10,7 @@ public abstract class CalculateBase {
     public double getRightVal() {return rightVal;}
     public void setRightVal(double rightVal) {this.rightVal = rightVal;}
     public double getResult() {return result;}
-    public void setResult(char result) {this.result = result;}
+    public void setResult(double result) {this.result = result;}
 
 //    public double getResult() {return result;}
 
