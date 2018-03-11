4073d6d17fcd28c0434442c3b016a64d52efbc0e
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-11 16:10:49 -0400

    added an abstract class.

--- src/com/company/MathEquation.java
+++ src/com/company/MathEquation.java
@@ -6,6 +6,15 @@ public class MathEquation {
     public double rightVal;
     public double result;
 
+    public double getLeftVal() {return leftVal;}
+    public void setLeftVal(double leftVal) {this.leftVal = leftVal;}
+    public double getRightVal() {return rightVal;}
+    public void setRightVal(double rightVal) {this.rightVal = rightVal;}
+    public char getOpCode() {return opCode;}
+    public void setOpCode(char opCode) {this.opCode = opCode;}
+
+    public double getResult() {return result;}
+
     public MathEquation() {}
 
     public MathEquation(char opCode) {
@@ -16,10 +25,6 @@ public class MathEquation {
         this(opCode);
         this.leftVal = leftVal;
         this.rightVal = rightVal;
-    }
-
-    public double getResult() {
-        return result;
     }
 
     public void execute(double leftVal, double rightVal) {
