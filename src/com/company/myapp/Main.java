16cf94b4cf4adc47b96f67b488ea60d2c8a62f9b
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-19 17:18:03 -0400

    moved the main class to its own package.

--- src/com/company/myapp/Main.java
+++ src/com/company/myapp/Main.java
@@ -1,4 +1,13 @@
-package com.company;
+package com.company.myapp;
+
+import com.company.Adder;
+import com.company.CalculateBase;
+import com.company.CalculateHelper;
+import com.company.Divider;
+import com.company.InvalidStatementException;
+import com.company.MathEquation;
+import com.company.Multiplier;
+import com.company.Subtracter;
 
 public class Main {
 
